6f836f36386a16cb264e0525c952e4cdfbe25c6a
rogers.machara <rogersmachara13@gmail.com>
2024-05-08 11:27:49 +0300

    user interfaces

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <main>
+    <h1>Hi, I'm brimin</h1>
+    <img alt="My profile pic" src="file:///home/briimiin/Downloads/asap.jpeg" />
+      <h2>About Me</h2>
+      <p>
+       A software developer who is intrested in sace facts and navigation
+      </p>
+      <div>
+        <a href="https://github.com">GitHub</a>
+        <a href="https://linkedin.com">LinkedIn</a>
+      </div>
+    </main>
+  );
 }
 
 export default App;
